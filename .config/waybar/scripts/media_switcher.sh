0868a618fb51d4e68046a72a894ed0fefd5f7c62
logan1o1 <saswatm2706@gmail.com>
2026-08-19 22:38:39 +0530

    updated waybar and wofi

--- .config/waybar/scripts/media_switcher.sh
+++ .config/waybar/scripts/media_switcher.sh
@@ -1,4 +1,19 @@
+#!/bin/zsh
 
-if [ "playerctl status" ]; then 
-        command ...
+if playerctl -p playerctld status >/dev/null 2>&1; then 
+        echo ""
 fi
+
+
+
+# "custom/media-switcher": {
+#         "format": "󰽛",
+#         "exec-if": "~/dotfiles/.config/waybar/scripts/media_switcher.sh",
+#         "on-click": "playerctld shift",
+#         "tooltip": false,
+#         "interval": 5
+# }
+# ```
+# ```sh
+# #!/bin/sh
+# playerctl -p playerctld status >/dev/null 2>&1 || exit 1
