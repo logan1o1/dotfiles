e3a1985270ac0afcac2a26a30c30fc73c7f7a0f9
logan1o1 <saswatm2706@gmail.com>
2026-09-17 22:07:01 +0530

    Added a plugin to yazi

--- .config/hypr/modules/generated_colors.lua
+++ .config/hypr/modules/generated_colors.lua
@@ -8,55 +8,55 @@ return {
     
     inverse_on_surface = "0xff2d3135",
     
-    inverse_primary = "0xff276389",
+    inverse_primary = "0xff246488",
     
-    inverse_surface = "0xffe0e3e8",
+    inverse_surface = "0xffdfe3e8",
     
-    on_background = "0xffe0e3e8",
+    on_background = "0xffdfe3e8",
     
     on_error = "0xff690005",
     
     on_error_container = "0xffffdad6",
     
-    on_primary = "0xff00344e",
+    on_primary = "0xff00344d",
     
-    on_primary_container = "0xffcae6ff",
+    on_primary_container = "0xffc8e6ff",
     
-    on_primary_fixed = "0xff001e30",
+    on_primary_fixed = "0xff001e2f",
     
-    on_primary_fixed_variant = "0xff004b70",
+    on_primary_fixed_variant = "0xff004c6e",
     
-    on_secondary = "0xff22323f",
+    on_secondary = "0xff21323f",
     
     on_secondary_container = "0xffd3e5f5",
     
-    on_secondary_fixed = "0xff0c1d29",
+    on_secondary_fixed = "0xff0b1d29",
     
     on_secondary_fixed_variant = "0xff384956",
     
-    on_surface = "0xffe0e3e8",
+    on_surface = "0xffdfe3e8",
     
     on_surface_variant = "0xffc1c7ce",
     
-    on_tertiary = "0xff362b4b",
+    on_tertiary = "0xff352b4b",
     
-    on_tertiary_container = "0xffebddff",
+    on_tertiary_container = "0xffe9ddff",
     
-    on_tertiary_fixed = "0xff201634",
+    on_tertiary_fixed = "0xff1f1635",
     
-    on_tertiary_fixed_variant = "0xff4d4162",
+    on_tertiary_fixed_variant = "0xff4b4163",
     
     outline = "0xff8b9198",
     
     outline_variant = "0xff41474d",
     
-    primary = "0xff96cdf8",
+    primary = "0xff94cdf7",
     
-    primary_container = "0xff004b70",
+    primary_container = "0xff004c6e",
     
-    primary_fixed = "0xffcae6ff",
+    primary_fixed = "0xffc8e6ff",
     
-    primary_fixed_dim = "0xff96cdf8",
+    primary_fixed_dim = "0xff94cdf7",
     
     scrim = "0xff000000",
     
@@ -70,11 +70,11 @@ return {
     
     shadow = "0xff000000",
     
-    source_color = "0xff2d75a2",
+    source_color = "0xff85c8f7",
     
     surface = "0xff101417",
     
-    surface_bright = "0xff363a3e",
+    surface_bright = "0xff353a3e",
     
     surface_container = "0xff1c2024",
     
@@ -84,21 +84,21 @@ return {
     
     surface_container_low = "0xff181c20",
     
-    surface_container_lowest = "0xff0b0f12",
+    surface_container_lowest = "0xff0a0f12",
     
     surface_dim = "0xff101417",
     
-    surface_tint = "0xff96cdf8",
+    surface_tint = "0xff94cdf7",
     
     surface_variant = "0xff41474d",
     
-    tertiary = "0xffcfc0e8",
+    tertiary = "0xffcec0e8",
     
-    tertiary_container = "0xff4d4162",
+    tertiary_container = "0xff4b4163",
     
-    tertiary_fixed = "0xffebddff",
+    tertiary_fixed = "0xffe9ddff",
     
-    tertiary_fixed_dim = "0xffcfc0e8",
+    tertiary_fixed_dim = "0xffcec0e8",
     
 }
 
---
+++ .config/yazi/init.lua
@@ -0,0 +1,20 @@
+require("bunny"):setup({
+  hops = {
+    { key = "/",          path = "/",                                    },
+    { key = "t",          path = "/tmp",                                 },
+    { key = "~",          path = "~",              desc = "Home"         },
+    { key = "m",          path = "~/Music",        desc = "Music"        },
+    { key = "p",          path = "~/projects/",    desc = "Projects"     },
+    { key = "d",          path = "~/dotfiles/.config",      desc = "Dotfiles" },
+    { key = "w",          path = "~/wallpapers/",  desc = "Wallpapers"   },
+    { key = { "l", "s" }, path = "~/.local/share", desc = "Local share"  },
+    { key = { "l", "b" }, path = "~/.local/bin",   desc = "Local bin"    },
+    { key = { "l", "t" }, path = "~/.local/state", desc = "Local state"  },
+    -- key and path attributes are required, desc is optional
+  },
+  desc_strategy = "path", -- If desc isn't present, use "path" or "filename", default is "path"
+  ephemeral = true, -- Enable ephemeral hops, default is true
+  tabs = true, -- Enable tab hops, default is true
+  notify = false, -- Notify after hopping, default is false
+  fuzzy_cmd = "fzf", -- Fuzzy searching command, default is "fzf"
+})
